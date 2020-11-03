b1e2802522a9eb03d111aaf6aad8cb7c69984900
hyesuhong <70276660+hyesuhong@users.noreply.github.com>
2020-11-04 00:26:59 +0900

    path change(css,js)

--- js/color_change.js
+++ js/color_change.js
@@ -3,15 +3,30 @@
 const body = document.getElementsByTagName('body')[0];
 const input_color = document.getElementById('COLOR_CODE');
 const change_btn = document.getElementById('CHANGE_BTN');
+const alert_text = document.getElementById('ALERT_AREA');
 
-change_btn.addEventListener('click', function () {
+// find G-Z g-z ' '
+const regPattern = /[G-Z|\s]/gi;
+
+function change_BG() {
   let color_code = input_color.value;
+  let color_check = regPattern.test(color_code);
 
-  color_code[0] === '#'
-    ? (color_code = color_code)
-    : (color_code = `#${color_code}`);
+  if(color_check) {
+    // not a code
+    alert_text.style.display = 'block';
+  } else {
+    alert_text.style.display = 'none';
+    body.style.background = `#${color_code}`;
+  };
+}
 
-  body.style.backgroundColor = color_code;
+input_color.addEventListener('keyup', (event) => {
+  if(event.keyCode === 13) {
+    change_BG();
+  }
 });
 
-// Add HEX Code validation
+change_btn.addEventListener('click', () => {
+  change_BG();
+});
